c8c4b3d4715f12ddeeebddf7a36a7b560171779e
Isaac Leong <isaacleong23367@gmail.com>
2025-12-27 15:07:34 +0800

    Updated CMakeLists.txt for Correct Linking during Compilation

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <Metal/Metal.hpp>
+#include <iostream>
+
+#define NS_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+
+int main() { std::cout << "Hello World!\n"; }
